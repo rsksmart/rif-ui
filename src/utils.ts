56c8451ce06dbff5e24cf07064e4d5447d9c81ea
Alvaro Fariña <farinalvaro@gmail.com>
2020-06-11 10:24:21 -0300

    fix: took rest of the string on shortenAddress

--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,4 @@
-export const shortenAddress = (address: string): string => `${address.substr(0, 6)}...${address.substr(address.length - 4 - 1, 4)}`
+export const shortenAddress = (address: string): string => `${address.substr(0, 6)}...${address.substr(address.length - 4)}`
 
 export const removeEmptySpaces = (str: string): string => str.replace(/\s/g, '')
 
