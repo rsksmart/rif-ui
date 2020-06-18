b2d8a44aa08617db2a52ec30c6141dbcf10830f9
Alvaro Fariña <farinalvaro@gmail.com>
2020-06-18 10:39:32 -0300

    test: added unit tests for molecules, organisms and templates

---
+++ src/components/organisms/organisms.test.tsx
@@ -0,0 +1,7 @@
+import { Footer } from '.'
+
+describe('Footer component sanity', () => {
+  it('is truthy', () => {
+    expect(Footer).toBeTruthy()
+  })
+})
---
+++ src/components/templates/templates.test.tsx
@@ -0,0 +1,13 @@
+import { FAQPageTemplate, PageTemplate } from '.'
+
+describe('FAQPageTemplate component sanity', () => {
+  it('is truthy', () => {
+    expect(FAQPageTemplate).toBeTruthy()
+  })
+})
+
+describe('PageTemplate component sanity', () => {
+  it('is truthy', () => {
+    expect(PageTemplate).toBeTruthy()
+  })
+})
