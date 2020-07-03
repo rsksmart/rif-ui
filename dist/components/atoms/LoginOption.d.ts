c3448d5e941bd7e5d63498d90b8e5566b0807098
Alvaro Fariña <farinalvaro@gmail.com>
2020-07-03 17:16:20 -0300

    feat: added tooltip to warn about multiple wallets

--- dist/components/atoms/LoginOption.d.ts
+++ dist/components/atoms/LoginOption.d.ts
@@ -1,8 +1,13 @@
-import React, { FC } from 'react';
+import { FC } from 'react';
+import ProviderInfo from '../../models/ProviderInfo';
 export interface LoginOptionProps {
     className?: string;
-    onClick: () => void;
-    content: React.ReactElement;
+    onClick?: () => void;
+    providerInfo?: ProviderInfo;
 }
+/**
+ * If not providerInfo is received, we assume there's no provider available and so suggest to install Nifty waller
+ * Same happens when no onClick is provided. We open the Nifty page on a new tab
+ */
 declare const LoginOption: FC<LoginOptionProps>;
 export default LoginOption;
