82106947bb6a533c2a253a5290e8779ac563f816
Alvaro Fariña <farinalvaro@gmail.com>
2020-07-06 11:07:44 -0300

    chore: removed dist folder

--- dist/components/molecules/FAQSection.d.ts
+++
@@ -1,9 +0,0 @@
-import { FC } from 'react';
-export interface FAQSectionProps {
-    answer: string;
-    initiallyExpanded?: boolean;
-    question: string;
-    className?: string;
-}
-declare const FAQSection: FC<FAQSectionProps>;
-export default FAQSection;
