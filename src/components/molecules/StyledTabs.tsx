b5eeda633d12da23cda706a6469e20d8af03f9dc
Alvaro Fariña <farinalvaro@gmail.com>
2020-08-12 17:43:38 -0300

    feat: adds styled nav tab and styled tabs components

---
+++ src/components/molecules/StyledTabs.tsx
@@ -0,0 +1,32 @@
+import React, { FC } from 'react'
+import Tabs, { TabsProps } from '@material-ui/core/Tabs'
+import { makeStyles } from '@material-ui/core/styles'
+import { colors } from '../../theme'
+
+export interface StyledTabsProps {
+  value: string
+  onChange: (event: React.ChangeEvent<{}>, newValue: string) => void
+}
+
+const useStyles = makeStyles({
+  indicator: {
+    display: 'flex',
+    justifyContent: 'center',
+    backgroundColor: 'transparent',
+  },
+  root: {
+    borderBottom: `1px solid ${colors.gray3}`,
+  },
+})
+
+const StyledTabs: FC<TabsProps> = (props: TabsProps) => {
+  const { children } = props
+  const classes = useStyles()
+  return (
+    <Tabs classes={classes} {...props}>
+      {children}
+    </Tabs>
+  )
+}
+
+export default StyledTabs
