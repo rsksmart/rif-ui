f138de8006d0f7d2f2baa7f56ecc6f83ace39142
Alvaro Fariña <farinalvaro@gmail.com>
2020-08-18 12:10:48 -0300

    chore: updates package.json version and removes dist folder

--- dist/assets/images/logos/index.d.ts
+++
@@ -1,4 +0,0 @@
-import logoBlackAndBlue from './logoBlackAndBlue.svg';
-import logoFullWhite from './logoFullWhite.svg';
-import rskLogo from './rskLogo.svg';
-export { logoBlackAndBlue, logoFullWhite, rskLogo, };
