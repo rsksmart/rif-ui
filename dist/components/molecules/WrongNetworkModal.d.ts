3dcac4233728137ece0b07375857b2801a4166b4
Alvaro Fariña <farinalvaro@gmail.com>
2020-10-01 11:58:55 -0300

    chore: ignores dist folder

--- dist/components/molecules/WrongNetworkModal.d.ts
+++
@@ -1,9 +0,0 @@
-import { FC } from 'react';
-export interface WrongNetworkModalProps {
-    open: boolean;
-    currentNetworkName?: string;
-    requiredNetworkName: string;
-    onClose: () => void;
-}
-declare const WrongNetworkModal: FC<WrongNetworkModalProps>;
-export default WrongNetworkModal;
--- dist/components/organisms/Header/Header.d.ts
+++
@@ -1,4 +0,0 @@
-import { FC } from 'react';
-import { HeaderProps } from './HeaderProps';
-declare const Header: FC<HeaderProps>;
-export default Header;
